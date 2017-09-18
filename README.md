7c4bda1b8d60d11609db2389b0dbacdfb6a899fb
Taha Malik <taha.malik87@gmail.com>
2017-09-18 14:11:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Movie trailer website
+
+The movie trailer website is a one page catalogue of movies where viewers can see a list of popular movies , view the art work and conveniently watch the trailer of each movie while staying on the same website.
+
+
+## Requirements
+
+This project is built on the python programming language. Users must have the latest python distribution installed on their system. Python download and installation instructions can be found at https://www.python.org/downloads/
+
+This project also relies on Python module : `Fresh_tomatoes.py` , this module file is included in the source code of the project and it must be present inside the project directory in order for the website to function properly
+
+## Getting started
+
+Download/Clone the project repository from : 
+
+Navigate into the project directory , open the file `fresh_tomatoes.html` in a web browser.
+
+Click on any of the movie artwork to start playing the trailer for that movie. 
+
+## Known bugs
+
+There are no known bug in this project at this time.
+
+
+
+
---
+++ entertainment_center.py
@@ -0,0 +1,17 @@
+# import the required classes and modules to run this application
+
+import Media
+import fresh_tomatoes
+
+# Create instances of Media class by adding movies to be displayed on webpage
+
+Moana = Media.Movie("Moana", "https://upload.wikimedia.org/wikipedia/en/thumb/2/26/Moana_Teaser_Poster.jpg/220px-Moana_Teaser_Poster.jpg", "https://www.youtube.com/watch?v=LKFuXETZUsI")
+Themummy = Media.Movie("The Mummy", "https://upload.wikimedia.org/wikipedia/en/a/a2/The_Mummy_%282017%29.jpg", "https://www.youtube.com/watch?v=IjHgzkQM2Sg")
+Legomovie = Media.Movie("The Lego Batman Movie", "https://upload.wikimedia.org/wikipedia/en/6/61/The_Lego_Batman_Movie_PromotionalPoster.jpg", "https://www.youtube.com/watch?v=rGQUKzSDhrg")
+Diverted = Media.Movie("Diverted", "https://upload.wikimedia.org/wikipedia/en/6/60/Diverted.jpg", "https://www.youtube.com/watch?v=9U3pKQbtuWE")
+
+# Add movies to array in order to be passed as argument to fresh_tomatoes.open_movies_page
+movies = [Moana, Themummy, Legomovie, Diverted]
+
+# Pass movies array to fresh_tomatoes as an argument
+fresh_tomatoes.open_movies_page(movies)
